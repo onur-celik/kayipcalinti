21f15d43341cba21f0c7f2804f019273dc9c2bba
Onur Celik <onur.celik@logsign.net>
2012-10-18 11:41:00 +0300

    ready2Go files uploaded

---
+++ header.php
@@ -0,0 +1,21 @@
+ <!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ready2go title</title>
+        <!-- css files -->
+        <link rel="stylesheet" href="CSS/reset.css" />
+        <link rel="stylesheet" href="CSS/bootstrap.min.css" />
+        <link rel="stylesheet" href="CSS/style.css" />
+        
+        <!-- javaScript files -->
+        <script type="text/javascript" src="JS/jquery.min.js"></script>
+        <script type="text/javascript" src="JS/jquery.easing.js"></script>
+        <script type="text/javascript" src="JS/jquery.color.js"></script>
+        <script type="text/javascript" src="JS/jquery.typewriter.js"></script>
+        <script type="text/javascript" src="JS/jquery.transit.min.js"></script>
+        <script type="text/javascript" src="JS/bootstrap.min.js"></script>
+        <script type="text/javascript" src="JS/scripts.js"></script>
+    </head>
+
+    <body> <!-- ends in footer.php -->
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include "header.php";  ?>
+        <p id="typist"></p>
+        <a href="README.md">See Readme File</a>
+        <div id="box"> <!-- this is gonna rotate 30degs on hover -->
+            <h1 id="heading">ready2Go</h1>
+        </div>
+        
+<?php include "footer.php";  ?>
