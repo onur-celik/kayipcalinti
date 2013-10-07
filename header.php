af01177b51fc8527ca8f8ca96fffc917ef5f7fbf
Onur Celik <onurcelik@me.com>
2013-10-07 20:33:06 +0300

    hede

--- header.php
+++ header.php
@@ -3,18 +3,34 @@
         <meta charset="UTF-8">
         <title>kayipcalinti.net | Kayıp Çalıntı Veritabanı</title>
         <!-- css files -->
-        <link rel="stylesheet" href="CSS/reset.css" />
-        <link rel="stylesheet" href="CSS/bootstrap.min.css" />
-        <link rel="stylesheet" href="CSS/style.css" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet/less" href="css/style.less" />
         
         <!-- javaScript files -->
-        <script type="text/javascript" src="JS/jquery.min.js"></script>
-        <script type="text/javascript" src="JS/jquery.easing.js"></script>
-        <script type="text/javascript" src="JS/jquery.color.js"></script>
-        <script type="text/javascript" src="JS/jquery.typewriter.js"></script>
-        <script type="text/javascript" src="JS/jquery.transit.min.js"></script>
-        <script type="text/javascript" src="JS/bootstrap.min.js"></script>
-        <script type="text/javascript" src="JS/scripts.js"></script>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.js"></script>
+        <script type="text/javascript" src="js/jquery.color.js"></script>
+        <script type="text/javascript" src="js/jquery.typewriter.js"></script>
+        <script type="text/javascript" src="js/jquery.transit.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/scripts.js"></script>
+        <script type="text/javascript" src="js/lesscss.js"></script>
     </head>
 
     <body> <!-- ends in footer.php -->
+
+    <header>
+	    <div class="row">
+		    <div class="col-sm-5">
+			    <h4>Kayıp Çalıntı Veritabanı</h4>
+		    </div>
+		    <div class="col-sm-2 pull-right text-right">
+		    	<button class="btn btn-info btn-sm">
+			    	<i class='glyphicon glyphicon-log-in'></i> Giriş
+		    	</button>
+		    	<button class="btn btn-primary btn-sm">
+			    	<i class='glyphicon glyphicon-user'></i> Kayıt
+		    	</button>
+		    </div>
+	    </div>
+    </header>
--- index.php
+++ index.php
@@ -1,4 +1,113 @@
 <?php include "header.php";  ?>
-<h1>kayipcalinti.net | Kayıp Çalıntı Veritabanı</h1>
+
+
+<div class="row" id="ui_container">
+	
+	<!-- KAYIPLAR -->
+	<div class="col-sm-6" id="kayiplarContainer">
+		<h4>KAYIPLAR</h4>
+		<hr />
+		<div class="row">
+			<div class="col-sm-3 categoryList">
+				<ul>
+					<li>
+						Elektronik
+						<ul>
+							<li>Cep Telefonu</li>
+							<li>Bilgisayar</li>
+							<li>Diğer</li>
+						</ul>
+					</li>
+					
+					<li>
+						Canlı
+						<ul>
+							<li>İnsan</li>
+							<li>Hayvan</li>
+						</ul>
+					</li>
+					<li>
+						Taşıt
+						<ul>
+							<li>Otomobil</li>
+							<li>Motorsiklet</li>
+							<li>Bisiklet</li>
+							<li>Diğer</li>
+						</ul>
+					</li>
+					<li>
+						Önemli Evrak
+						<ul>
+							<li>Kimlik</li>
+							<li>Pasaport</li>
+							<li>Ehliyet</li>
+							<li>Diğer</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<div class="col-sm-9">
+				resimler
+				<br />
+				<button class="btn btn-link btn-xs"><i class="glyphicon glyphicon-chevron-right"></i> Tümünü Gör</button>
+			</div>
+		</div>
+	</div>
+	<!-- KAYIPLAR / end-->
+	
+	<!-- BULUNANLAR -->
+	<div class="col-sm-6" id="bulunanlarContainer">
+		<h4>BULUNANLAR</h4>
+		<hr />
+		<div class="row">
+			<div class="col-sm-3 categoryList">
+				<ul>
+					<li>
+						Elektronik
+						<ul>
+							<li>Cep Telefonu</li>
+							<li>Bilgisayar</li>
+							<li>Diğer</li>
+						</ul>
+					</li>
+					
+					<li>
+						Canlı
+						<ul>
+							<li>İnsan</li>
+							<li>Hayvan</li>
+						</ul>
+					</li>
+					<li>
+						Taşıt
+						<ul>
+							<li>Otomobil</li>
+							<li>Motorsiklet</li>
+							<li>Bisiklet</li>
+							<li>Diğer</li>
+						</ul>
+					</li>
+					<li>
+						Önemli Evrak
+						<ul>
+							<li>Kimlik</li>
+							<li>Pasaport</li>
+							<li>Ehliyet</li>
+							<li>Diğer</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<div class="col-sm-9">
+				resimler
+				<br />
+				<button class="btn btn-link btn-xs"><i class="glyphicon glyphicon-chevron-right"></i> Tümünü Gör</button>
+			</div>
+		</div>
+	</div>
+	<!-- BULUNANLAR / end -->
+</div>
+
+
         
 <?php include "footer.php";  ?>
